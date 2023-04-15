c3cacb3140231078c05617573b6d73a66026fa11
Oskarnn <nygren.a.oskar@gmail.com>
2023-04-15 23:05:15 +0200

    commit stationär

--- astro-proj/src/components/Horoscope.js
+++ astro-proj/src/components/Horoscope.js
@@ -1,27 +1,42 @@
 import Navbar from "./Navbar";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import React from "react";
+import axios from 'axios';
 
 // useEffect
 const Horoscope = () => {
 
-    const [signs, setSigns] = useState( [
-        { id: 1, name: "Aries"},
-        { id: 2, name: "Aries"},
-        { id: 3, name: "Aries"},
-    ]);
+    const [signs, setSigns] = useState([]);
+
+   useEffect(() => {
+    fetch()
+   },[]) //  [] dependency array, gör så att useEffect kör funktioner vid första renderingen. (Bra för t.ex fetch)
+//går att lägga in tex namn på variabler som gör att funktionen körs om värdet ändras i den variabeln.
+  
+
+function fetch() {
+    axios.get('http://localhost:8000/data')
+    .then(response => {
+        setSigns(response.data)
+        console.log(signs[1].pic)
+    })
+   }
+
+   let toggle = false;
 
     return (
         <div className="contentBox">
             <Navbar/>
             <div className="horoSection">
 
-                    {signs.map((sign) => (
+                    { signs.map((sign) => (
                         <div className="horoList" key={sign.id}>
-                            <p> { sign.name }</p>
-                            <p> { sign.id }</p>
+                            
+                            <img src={ sign.pic } alt={sign.sign} />
+                            <button onClick={ () => toggle = true }> { sign.sign }</button>
                         </div>
-                    ))}
+
+                        )) }
 
 
             </div>
